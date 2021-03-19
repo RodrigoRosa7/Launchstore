2c40080503846cf868ba3b0bedefd2fe3c551420
Rodrigo <irodrigo.rosa@gmail.com>
2021-03-18 22:36:39 -0300

    Gerenciamento de imagens no backend e front

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,6 +1,7 @@
 const {formatPrice} = require('../../lib/utils')
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+const File = require('../models/File')
 
 module.exports = {
   create(req, res){
@@ -24,8 +25,14 @@ module.exports = {
         return res.send("Preencha todos os campos corretamente")
     }
 
+    if(req.files.length == 0)
+      return res.send("Por favor inclua pelo menos uma imagem!")
+
     let results = await Product.create(req.body)
     const productId = results.rows[0].id
+
+    const filesPromise = req.files.map(file => File.create({...file, product_id: productId}))
+    await Promise.all(filesPromise)
 
     return res.redirect(`/products/${productId}`)
   },
@@ -39,23 +46,48 @@ module.exports = {
     product.old_price = formatPrice(product.old_price)
     product.price = formatPrice(product.price)
 
+    //get categories
     results = await Category.all()
     const categories = results.rows
 
-    return res.render('products/edit.njk', {product, categories})
+    //get files
+    results = await Product.files(product.id)
+    let files = results.rows
+    files = files.map(file => ({
+      ...file,
+      src:`${req.protocol}://${req.headers.host}${file.path.replace("public","")}`
+    }))
+
+    return res.render('products/edit.njk', {product, categories, files})
   },
 
   async put(req, res){
     const keys = Object.keys(req.body)
 
     for(const key of keys){
-      if(req.body[key] == "")
+      if(req.body[key] == "" && key != "removed_files")
         return res.send("Preencha todos os campos corretamente")
+    }
+
+    if(req.files.length > 0) {
+      const newFilesPromise = req.files.map(file => File.create({...file, product_id: req.body.id}))
+
+      await Promise.all(newFilesPromise)
+    }
+
+    if(req.body.removed_files){
+      const removedFiles = req.body.removed_files.split(",") // [1,2,3,]
+      const lastIndex = removedFiles.length - 1
+      removedFiles.splice(lastIndex, 1) // [1,2,3]
+
+      const removedFilesPromises = removedFiles.map(id => File.delete(id))
+
+      await Promise.all(removedFilesPromises)
     }
 
     req.body.price = req.body.price.replace(/\D/g,"")
 
-    if(req.body.old_price != req.body){
+    if(req.body.old_price != req.body.price){
       const oldProduct = await Product.find(req.body.id)
 
       req.body.old_price = oldProduct.rows[0].price
